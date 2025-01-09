36c881910be7aa1d1d3ec092510c8933d0b67009
Martin Paucot <contact@martin-paucot.fr>
2025-01-09 05:49:03 +0100

    feat(maintenance): create maintenance package (#19)

---
+++ packages/maintenance/src/maintenance_manager.ts
@@ -0,0 +1,24 @@
+import { RuntimeException } from '@adonisjs/core/exceptions'
+import { ManagerModeFactory as ManagerDriverFactory } from './types.js'
+
+export class MaintenanceManager<KnownDrivers extends Record<string, ManagerDriverFactory>> {
+  #cachedDrivers: Map<keyof KnownDrivers, ReturnType<KnownDrivers[keyof KnownDrivers]>> = new Map()
+
+  constructor(public config: { default?: keyof KnownDrivers; drivers: KnownDrivers }) {}
+
+  driver<DriverName extends keyof KnownDrivers>(driver?: DriverName) {
+    const driverToUse = (driver || this.config.default) as keyof KnownDrivers
+    if (!driverToUse) throw new RuntimeException('No search engine selected')
+
+    if (this.#cachedDrivers.has(driverToUse)) {
+      return this.#cachedDrivers.get(driverToUse)!
+    }
+
+    /**
+     * Otherwise create a new instance and cache it
+     */
+    const newEngine = this.config.drivers[driverToUse]() as ReturnType<KnownDrivers[DriverName]>
+    this.#cachedDrivers.set(driverToUse, newEngine)
+    return newEngine
+  }
+}
