94ab3cdc6bb5abb41c067cec9b9a3cab4b8d68f9
Martin PAUCOT <contact@martin-paucot.fr>
2025-03-31 10:48:29 +0200

    update documentation

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -17,55 +17,6 @@ export const baseOptions: BaseLayoutProps = {
   },
   links: [
     {
-      type: 'menu',
-      text: 'Packages',
-      url: '/docs',
-      items: [
-        {
-          icon: <Server />,
-          text: 'OpenAPI',
-          description: 'Generate compliant OpenAPI V3 specification using decorators',
-          url: '/docs/openapi',
-        },
-        {
-          icon: <Braces />,
-          text: 'GraphQL',
-          description: 'Create GraphQL APIs using your favorite Framework',
-          url: '/docs/graphql',
-        },
-        {
-          icon: <CircleDollarSign />,
-          text: 'Shopkeeper',
-          description: "Fluent interface to Stripe's subscription billing services",
-          url: '/docs/shopkeeper',
-        },
-        {
-          icon: <Search />,
-          text: 'Magnify',
-          description: 'Add Full-Text Search to your Lucid models',
-          url: '/docs/magnify',
-        },
-        {
-          icon: <Vault />,
-          text: 'Crypt',
-          description: 'Safely store your secrets in your repository',
-          url: '/docs/crypt',
-        },
-        {
-          icon: <Vault />,
-          text: 'Maintenance',
-          description: 'Easily put your application in maintenance mode',
-          url: '/docs/maintenance',
-        },
-      ],
-    },
-    {
-      icon: <AlbumIcon />,
-      text: 'Blog',
-      url: '/blog',
-      active: 'nested-url',
-    },
-    {
       type: 'custom',
       children: <GithubInfo owner="FriendsOfAdonis" repo="FriendsOfAdonis" />,
     },
