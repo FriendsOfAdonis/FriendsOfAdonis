b92df4eb481755d873c7aaa3309ef7231f0801c8
Martin PAUCOT <contact@martin-paucot.fr>
2025-03-31 10:10:04 +0200

    fix eslint configuration

---
+++ docs/eslint.config.mjs
@@ -0,0 +1,15 @@
+import next from '@foadonis/eslint-config/next.app.js'
+
+export default [
+  {
+    ignores: ['.source/*'],
+  },
+  ...next,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.lint.json',
+      },
+    },
+  },
+]
