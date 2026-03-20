65938653630fdf081ebaeeb395b1b58767da739b
Martin PAUCOT <contact@martin.xyz>
2026-03-20 22:39:48 +0100

    ci: add preview.yml to auto-release on PR with pkg-pr-new

--- packages/openapi/src/openapi.ts
+++ packages/openapi/src/openapi.ts
@@ -50,6 +50,7 @@ export class OpenAPI {
    * @returns OpenAPI compliant document.
    */
   async buildDocument() {
+    console.log('Test for release')
     if (this.#document && this.#isProduction) {
       return this.#document
     }
