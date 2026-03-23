4e3595542363ea06804151d4a93180e388861fe6
densetsuuu <raph.alarcon1@gmail.com>
2026-03-23 18:26:08 +0100

    style(shopkeeper): run prettier/eslint format

--- packages/shopkeeper/src/mixins/prorates.ts
+++ packages/shopkeeper/src/mixins/prorates.ts
@@ -4,51 +4,51 @@ import { type Constructor } from '../types.js'
 export function prorates() {
   return <Model extends Constructor>(superclass: Model) => {
     return class ProratesImpl extends superclass {
-    /**
-     * Indicates if the price change should be prorated.
-     */
-    #prorationBehaviour: Stripe.SubscriptionCreateParams.ProrationBehavior = 'create_prorations'
+      /**
+       * Indicates if the price change should be prorated.
+       */
+      #prorationBehaviour: Stripe.SubscriptionCreateParams.ProrationBehavior = 'create_prorations'
 
-    /**
-     * Indicate that the price change should not be prorated.
-     */
-    noProrate(): this {
-      this.#prorationBehaviour = 'none'
-      return this
-    }
+      /**
+       * Indicate that the price change should not be prorated.
+       */
+      noProrate(): this {
+        this.#prorationBehaviour = 'none'
+        return this
+      }
 
-    /**
-     * Indicate that the price change should be prorated.
-     */
-    prorate(): this {
-      this.#prorationBehaviour = 'create_prorations'
-      return this
-    }
+      /**
+       * Indicate that the price change should be prorated.
+       */
+      prorate(): this {
+        this.#prorationBehaviour = 'create_prorations'
+        return this
+      }
 
-    /**
-     * Indicate that the price change should always be invoiced.
-     */
-    alwaysInvoice(): this {
-      this.#prorationBehaviour = 'always_invoice'
-      return this
-    }
+      /**
+       * Indicate that the price change should always be invoiced.
+       */
+      alwaysInvoice(): this {
+        this.#prorationBehaviour = 'always_invoice'
+        return this
+      }
 
-    /**
-     * Set the prorating behavior.
-     */
-    setProrationBehavior(
-      prorationBehaviour: Stripe.SubscriptionCreateParams.ProrationBehavior
-    ): this {
-      this.#prorationBehaviour = prorationBehaviour
-      return this
-    }
+      /**
+       * Set the prorating behavior.
+       */
+      setProrationBehavior(
+        prorationBehaviour: Stripe.SubscriptionCreateParams.ProrationBehavior
+      ): this {
+        this.#prorationBehaviour = prorationBehaviour
+        return this
+      }
 
-    /**
-     * Determine the prorating behavior when updating the subscription.
-     */
-    prorateBehavior(): Stripe.SubscriptionCreateParams.ProrationBehavior {
-      return this.#prorationBehaviour
-    }
+      /**
+       * Determine the prorating behavior when updating the subscription.
+       */
+      prorateBehavior(): Stripe.SubscriptionCreateParams.ProrationBehavior {
+        return this.#prorationBehaviour
+      }
     }
   }
 }
