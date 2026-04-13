e4ea0a9e8f7e8192be443472b84f975a00090030
Martin PAUCOT <contact@martin.xyz>
2026-04-13 07:29:23 +0200

    revert(openapi): remove lucid module

--- packages/openapi/modules/lucid/main.ts
+++
@@ -1,6 +0,0 @@
-export * from './utils.ts'
-
-export { DEFAULT_PROPERTY_OPTIONS_MAPPING } from './mapping.ts'
-export { default as DEFAULT_OPENAPI_SCHEMA_RULES } from './rules.ts'
-
-export type * from './types.ts'
--- packages/openapi/modules/lucid/types.ts
+++
@@ -1,3 +0,0 @@
-import { type ColumnInfo, type DatabaseColumn } from '@adonisjs/lucid/types/schema_generator'
-
-export type ColumnInfoFn = (dataType: string, column: DatabaseColumn) => ColumnInfo
