a50a96921861f0f9d7f326fbccffff778922dee1
densetsuuu <raph.alarcon1@gmail.com>
2026-05-07 15:55:52 +0200

    docs(shopkeeper): add migration guide, fix docs inaccuracies

--- packages/shopkeeper/index.ts
+++ packages/shopkeeper/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 
 export { Shopkeeper } from './src/shopkeeper.ts'
+export { Checkout } from './src/checkout.ts'
 
 export { defineConfig } from './src/define_config.js'
 export { configure } from './configure.js'
